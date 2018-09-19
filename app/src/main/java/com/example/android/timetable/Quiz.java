077e0143044dc08576ed23fbf853818f97c4e427
Canal Kim <canalkim@git.com>
2018-09-18 22:37:14 -0700

    Working on quiz activity

--- app/src/main/java/com/example/android/timetable/Quiz.java
+++ app/src/main/java/com/example/android/timetable/Quiz.java
@@ -2,6 +2,7 @@ package com.example.android.timetable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Quiz extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class Quiz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+    }
+
+    public void reset_quiz(View view) {
+
+    }
+
+    public void answerPressed(View view) {
+
     }
 }
